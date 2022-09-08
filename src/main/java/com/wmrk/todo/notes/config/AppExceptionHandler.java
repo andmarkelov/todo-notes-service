4fbda2935fb8691801cd8e32243e6350037bb1e7
wmrk <andrew.wamark@gmail.com>
2022-09-08 16:45:04 +0300

    made REST returns 404 instead of 500 if path variable was not found

---
+++ src/main/java/com/wmrk/todo/notes/config/AppExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.wmrk.todo.notes.config;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingPathVariableException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class AppExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleMissingPathVariable(MissingPathVariableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        return super.handleMissingPathVariable(ex, headers, HttpStatus.NOT_FOUND, request);
+    }
+
+}
